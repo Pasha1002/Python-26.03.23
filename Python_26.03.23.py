7d3bc46add018d22a3a6ccbea7d30431112070cd
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-01 17:21:49 +0300

    tusk dune

--- Python_26.03.23.py
+++ Python_26.03.23.py
@@ -1,1 +1,19 @@
 
+
+num_1  = int(input('Enter number 1: '))
+num_2  = int(input('Enter number 2: '))
+counter = 0
+for i in range(num_1, num_2+1):
+        print(i)
+print('\n')
+for i in range(num_2, num_1-1, -1):
+        print(i)
+print('\n')
+for i in range(num_1, num_2+1):
+     if i %7 ==0:
+         print(i)
+print('\n')
+for i in range(num_1, num_2+1):
+    if i %5 ==0:
+        counter += 1
+print(counter)
