7668091df5d2a84c8b57d89f72f2a33bb3321289
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-01 17:10:28 +0300

    tusk dune

--- Python_26.03.23.py
+++ Python_26.03.23.py
@@ -1,1 +1,9 @@
 
+num_1  = int(input('Enter number 1: '))
+num_2  = int(input('Enter number 2: '))
+if num_1 > num_2:
+    num_1,num_2=num_2,num_1  
+for i in range(num_1, num_2+1):
+    if i % 7 ==0:
+        print(i)
+ 
