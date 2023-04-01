e5e8aaa5c96fc4ac699b12ec5d13d12e8fbf5f76
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-01 17:47:35 +0300

    tusk dune

--- Python_26.03.23.py
+++ Python_26.03.23.py
@@ -1,1 +1,13 @@
 
+num_1  = int(input('Enter number 1: '))
+num_2  = int(input('Enter number 2: '))
+
+for i in range(num_1,num_2+1):
+    if i %3 ==0 %5 ==0:
+        print ("Fizz Buzz")
+    elif i %3 ==0:
+        print("Fizz")
+    elif i %5 ==0:
+        print("Buzz")
+    else:
+        print(i)
